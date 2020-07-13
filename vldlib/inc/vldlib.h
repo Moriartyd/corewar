3077106d0a6d1ff193dd6289881cb56c2a0cf2b6
Chella Pollich <cpollich@mi-o1.msk.21-school.ru>
2020-07-13 21:54:25 +0300

    Изменил t_op и добавил функции для создания и удаления структур

--- vldlib/inc/vldlib.h
+++ vldlib/inc/vldlib.h
@@ -6,7 +6,7 @@
 /*   By: cpollich <cpollich@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/10 17:37:16 by cpollich          #+#    #+#             */
-/*   Updated: 2020/07/13 19:05:54 by cpollich         ###   ########.fr       */
+/*   Updated: 2020/07/13 21:48:09 by cpollich         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,25 +31,48 @@
 #	define INV_NAME_M	"name invalid\n"
 #	define INV_NAME_L	13
 
-typedef struct  s_hero
-{
-	char        *name;
-	char        *comment;
-}               t_hero;
+/*
+**	Bytes - сколько байт занимает данная операция
+*/
 
 typedef struct	s_op
 {
 	int			code;
 	int			types[3];
 	int			args[3];
+	int			bytes;
+	char		**labels;
 	struct s_op	*prev;
 	struct s_op	*next;
 }				t_op;
 
-int				check_base_asm(int ac, char **av);
-int				check_asm(char *file, t_hero *hero);
+typedef struct  s_hero
+{
+	char        *name;
+	char        *comment;
+	t_op		*op;
+}               t_hero;
+
+/*
+**	* ft_read_until_ch	-	читает файл посимвольно до символа c, 
+**							возвращает количество прочитанного.
+**	* get_type			-	определяет тип считанной строки
+**							(сам модет читать строки)
+**	*
+*/
+
 int				ft_read_until_ch(int fd, int c, char **str);
 int				get_type(char *str, int bytes, int fd);
 int				check_namecomm(char *str, int type, int fd);
+
+/*
+**		MEMORY BLOCK
+*/
+
+t_hero			*init_hero(void);
+void			del_hero(t_hero *hero);
+t_op			*new_op(void)
+void			del_ops(t_op *op);
+
 
 #	endif
---
+++ vldlib/src/memory_op.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   memory_op.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cpollich <cpollich@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/13 20:21:24 by cpollich          #+#    #+#             */
+/*   Updated: 2020/07/13 21:29:28 by cpollich         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "vldlib.h"
+
+t_op	*new_op(void)
+{
+	t_op	*res;
+	int		i;
+
+	res = (t_op *)malloc(sizeof(t_op));
+	if (!res)
+		exit(-1);//Malloc Error
+	res->code = 0;
+	i = 0;
+	while (i < 3)
+	{
+		res->types[i] = 0;
+		res->args = 0;
+	}
+	res->labels = NULL;
+	res->prev = 0;
+	res->next = 0;
+	return (res);
+}
+
+void	del_ops(t_op *op)
+{
+	t_op	*tmp;
+
+	if (op)
+	{
+		while (op)
+		{
+			ft_doublestrdel(&(op->labels));
+			tmp = op->next;
+			free(op);
+			op = tmp;
+		}
+	}
+}
+
+/*
+** Парсит строку на инструкцию
+*/
+
+static void	fill_op(t_op *op, const char *str)
+{
+	
+}
+
+/*
+**	Добавляет в конец списка новую инструкцию
+*/
+
+void	add_op(t_op *op, const char *str)
+{
+	while (op->next)
+		op = op->next;
+	op->next = new_op();
+	op->next->prev = op;
+	op->next->next = NULL;
+	// fill_op(op->next, str);
+}
