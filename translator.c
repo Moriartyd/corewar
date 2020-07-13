be9d72f062c494a163837ab89a9b18c72517ca89
Dancy Dratini <ddratini@mi-o5.msk.21-school.ru>
2020-07-13 19:42:42 +0300

    write magic header

--- translator.c
+++ translator.c
@@ -6,14 +6,25 @@
 /*   By: ddratini <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/13 19:21:34 by ddratini          #+#    #+#             */
-/*   Updated: 2020/07/13 19:28:32 by ddratini         ###   ########.fr       */
+/*   Updated: 2020/07/13 19:40:19 by ddratini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vldlib/inc/vldlib.h"
 
+void		write_magicheader()
+{
+	int fd;
+	char *fname;
+
+	fname = "x.cor";
+	fd = open("name.cor", O_CREAT);////O_RDWR);
+	write(fd, "0xea83f3", 4 * 2);
+}
+
 int			translator(t_op op)
 {
+	write_magicheader();
 	printf("ASSMCHECK");
 	return (0);
 }
