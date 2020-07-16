8d3f6d86f1d303f682093f77e6805963bc4c3341
ddratini <ddratini@student.21-school.ru>
2020-07-16 17:35:45 +0300

    wrote mheader reverted to unsigned yo file

--- translator.c
+++ translator.c
@@ -12,18 +12,17 @@
 
 #include "vldlib/inc/vldlib.h"
 
-int		write_magicheader()
+int		write_magicheader(unsigned char *mh)
 {
 	int fd;
 	char *fname;
 	int retw;
-	char mh[4];
 
 	//mh[0] = COREWAR_EXEC_MAGIC
-	mh[0] = 3;
-	mh[1] = 15;
-	mh[2] = 3;
-	mh[3] = 8;
+	mh[0] = 243;//3;
+	mh[1] = 131;//15;
+	mh[2] = 234;//3;
+	mh[3] = 0;//8;
 	fname = "x.cor";
 //	fd = open("name1.cor", O_RDWR | O_TRUNC | O_CREAT , 0666);////O_RDWR);
 	fd = open("name2.cor", O_CREAT| O_TRUNC | O_RDWR , 0666);////O_RDWR);
@@ -38,7 +37,7 @@ int		write_magicheader()
 
 int			op_live(t_op op, int fd)
 {
-	char bc[1];
+	unsigned char bc[1];
 	char live;
 
 	live = 01;
@@ -55,11 +54,50 @@ int			detect_op(t_op op, int fd)
 	return (0);
 }
 
+void		ft_print_bits(unsigned char octet)//
+{
+	int i;
+	int buf;
+
+	i = 128;
+	while (i != 0)
+	{
+		buf = octet & i;
+		i /= 2;
+		if (buf > 0)
+		{
+			write(1, "1", 1);
+		}
+		else if (buf == 0)
+		{
+			write(1, "0", 1);
+		}
+	}
+	printf("\n");
+}
+
+int 		print_byte_int(int x)
+{
+	printf("MH=%d\n", x);
+	ft_print_bits(x);
+	ft_print_bits(x >> 8);//left byte 0;
+	ft_print_bits(x >> 16);
+	ft_print_bits(x >> 24);
+	ft_print_bits(x >> 31);
+	printf("\n");
+	return (0);
+}
 int			translator(t_op op)
 {
+	unsigned char bc[9999] = {0};
 	int fd;
+	int x;
 
-	fd = write_magicheader();
+	fd = write_magicheader(bc);
+	x = COREWAR_EXEC_MAGIC << 8;
+	//x = 1;
+	print_byte_int(x);
+	op.code = 1;
 	detect_op(op, fd);
 	printf("ASSMCHECK");
 	return (0);
