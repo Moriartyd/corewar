d8f5585c06cf31daceb1baad5376fe849f6092ef
ddratini <ddratini@student.21-school.ru>
2020-07-26 15:42:17 +0300

    .s champs

---
+++ asm/.r
@@ -0,0 +1,12 @@
+.name       "Batman"
+.comment "Arkham"
+
+	and%:live, r1, r3
+live:
+live:
+	live %1               #<-- На эту операцию указывает метка live
+	zjmp %1
+			live:	#ds a
+	pop:
+	sti r09, -2, %1
+		
